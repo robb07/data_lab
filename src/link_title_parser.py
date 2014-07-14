e016cca180ce599bd0030e7cc6968d27a2371419
robb07 <zap376@gmail.com>
2014-07-14 00:12:13 -0400

    Added a try except block to link parser to handle bad unicode characters.

--- src/link_title_parser.py
+++ src/link_title_parser.py
@@ -47,17 +47,35 @@ data_loc = 'D:/HTML_dump'
 
 
 site_to_parse_rules = dict([
-    ('npr',     [dict([('tag_names','a')]),
-                 dict([('tag_names','h1'),('attr_name','class'),('attr_vals',['title'])])]),
-    ('cnn',     [dict([('tag_names','div'),('attr_name','data-vr-zone'),('attr_vals',['t1','THE LATEST','OPINION','MORE TOP STORIES','IN CASE YOU MISSED IT','C1.5Top','C2'])]),
-                 dict([('tag_names','a')])]),
-    ('bbcnews', [dict([('tag_names','a'),('attr_name','class'),('attr_vals',['story'])])]),
-    ('abcnews', [dict([('tag_names','div'),('attr_name','class'),('attr_vals',['midcontainer'])]),
-                 dict([('tag_names','a')])]),
-    ('wired',   [dict([('tag_names','div'),('attr_name','class'),('attr_vals',['headline','feed-content'])]),
-                 dict([('tag_names','a')])]),
-    ('cbsnews', [dict([('tag_names',['h3','h4']),('attr_name','class'),('attr_vals',['title'])])]),
+#     ('npr',     [dict([('tag_names','a')]),
+#                  dict([('tag_names','h1'),('attr_name','class'),('attr_vals',['title'])])]),
+#     ('cnn',     [dict([('tag_names','div'),('attr_name','data-vr-zone'),('attr_vals',['t1','THE LATEST','OPINION','MORE TOP STORIES','IN CASE YOU MISSED IT','C1.5Top','C2'])]),
+#                  dict([('tag_names','a')])]),
+#     ('bbcnews', [dict([('tag_names','a'),('attr_name','class'),('attr_vals',['story'])])]),
+#     ('abcnews', [dict([('tag_names','div'),('attr_name','class'),('attr_vals',['midcontainer'])]),
+#                  dict([('tag_names','a')])]),
+#     ('wired',   [dict([('tag_names','div'),('attr_name','class'),('attr_vals',['headline','feed-content'])]),
+#                  dict([('tag_names','a')])]),
+#     ('cbsnews', [dict([('tag_names',['h3','h4']),('attr_name','class'),('attr_vals',['title'])])]),
+      ('reddit', [dict([('tag_names','a'),('attr_name','class'),('attr_vals',['title'])])]),
     ])
+
+bad_unicode = [u'\u200b',u'\xe1',u'\u2019',u'\u2018',
+                u'\xab',
+                u'\u2013',
+                u'\u201c',
+                u'\u201c',
+                u'\xe5',
+                u'\u2013',
+                u'\u201c',
+                u'\xbb',
+                u'\xd7',
+                u'\xd7',
+                u'\u201c',
+                u'\xe3',
+                u'\u201c',
+                u'\u2013',
+                u'\u272e']
 
 for site, parse_rules in site_to_parse_rules.iteritems():
     webpages = filter(lambda x: x.startswith(site), os.listdir(data_loc))
@@ -68,5 +86,9 @@ for site, parse_rules in site_to_parse_rules.iteritems():
     
     with open(os.path.join(data_loc,site+'_link_titles.txt'),'w') as f_out:
         for title in link_titles:
-            title = title.replace(u'\u200b','')
-            f_out.write(title+os.linesep)
+            for c in bad_unicode:
+                title = title.replace(c,'')
+            try:
+                f_out.write(title+os.linesep)
+            except UnicodeEncodeError as e:
+                print "UnicodeEncodeError: {0!r}".format(e)
